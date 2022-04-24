3edd1ce6674bee2902201d8c41e19b0edc2b15b7
jose654 <50951160+jose654@users.noreply.github.com>
2022-04-24 18:24:59 +0200

    added skills section

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var lastWidth=5000;
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 10){
@@ -10,4 +11,12 @@ $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
     });
+    /*  //oprava - pri sudych kliknuti na navbar menubtn potom pri zmenseni i=obrazovky se zobrazi menu
+    window.matchMedia().onchange(function(){
+        if(window.innerWidth < lastWidth && window.innerWidth < 931){
+            $('.navbar .menu').removeClass("active");
+        }
+        lastWidth = innerWidth;
+    });
+    */
 });
